336167254dfc5d69ca9fb78a1de1bbeb74d4ba16
G0rde3V <gordeev-5@yandex.ru>
2024-08-18 13:56:25 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -1,22 +1,19 @@
-calls = 0                                            # Создать переменную calls = 0 вне функций.
-# Функция count_calls подсчитывающая вызовы остальных функций.
-def count_calls(call):                               # Создать функцию count_calls и изменять в ней значение переменной calls.
-    global calls                                     # Используем переменную calls из глобального списка имен
-    calls += call                                    # Увеличиваем значение на указанное число (в данном случае на 1) call
-    return calls                                     # С помощью оператора return возвращаем данные calls после выполнения работы самой функции.
+calls = 0                                           
+def count_calls(call):                               
+    global calls                                     
+    calls += call                                    
+    return calls                                     
 
-# Функция string_info принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
-def string_info(string):                             # Создать функцию string_info с параметром string
-    a = []                                           # Создаем список [] переменной а
-    a.append(len(string))                            # Добавим в строку а и пределим длину вводимого значения строки
-    a.append(string.upper())                         # Строку а вывести в верхнем регистре
-    a.append(string.lower())                         # Строку а вывести в нижнем регистре
-    count_calls(1)                                   # Вызываем функцию count_calls(1)
-    return a                                         # Возвращаем данные строки а после выполнения работы самой функции.
-# Функция is_contains принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке,
-# False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
-def is_contains(string_info, is_contains):           # Создать функцию is_contains с двумя параметрами string и list_to_search
-    string_info.lower()                              # Принимаем первый аргумент
+def string_info(string):                             
+    a = []                                           
+    a.append(len(string))                            
+    a.append(string.upper())                         
+    a.append(string.lower())                         
+    count_calls(1)                                   
+    return a                                         
+
+def is_contains(string_info, is_contains):           
+    string_info.lower()                              
     new_is_contains = []
     count_calls(1)
     for i in is_contains:
